9563e5df9f39a8d1135c15357a7f4a0476af390d
Michel Bystranowski <michel@vertige.org>
2015-03-26 16:51:39 +0100

    commit initial - Généré par la Fabrique

---
+++ lang/paquet-newsletters_avancees_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // N
+    'newsletters_avancees_description' => 'Permet de choisir l\'ordre dans la sélection éditoriale de chaque newsletter.',
+    'newsletters_avancees_nom' => 'Newsletter avancées',
+    'newsletters_avancees_slogan' => 'Améliorer la composition de newletters sur base de sélections éditoriales.',
+);
